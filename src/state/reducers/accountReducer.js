64a1de32fde05080ae2b05bc30c6f34184b95f3c
YaqubRahman <YaqubRahman999@gmail.com>
2025-06-05 10:35:45 +0100

    feat: comments on reducers

--- src/state/reducers/accountReducer.js
+++ src/state/reducers/accountReducer.js
@@ -1,8 +1,14 @@
+// Takes the current state and an action as an argument
 const reducer = (state = 0, action) => {
+  // Checking the type property of the incoming action
   switch (action.type) {
     case "deposit":
+      // Returns a new state based on the action
+      // Does not mutate the original state
       return state + action.payload;
     case "withdraw":
+      // Returns a new state based on the action
+      // Does not mutate the original state
       return state - action.payload;
     default:
       return state;
--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
 import accountReducer from "./accountReducer";
 
+// This creates a root reducer by combining multiple reducers
 const reducers = combineReducers({
+  // Creating a state object with a key called account
+  // And let accountReducer manage the state under that key
   account: accountReducer,
 });
 
